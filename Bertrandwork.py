5dab7e522a15ab3023c621db7d44c45f83f706b1
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 13:02:11 +0300

    Update and rename Swartz.py to Bertrandwork.py

---
+++ Bertrandwork.py
@@ -0,0 +1,3 @@
+
+for _ in range(1000000):
+    print("Work is of two kinds: first, altering the position of matter at or near the earth's surface relative to other matter; second, telling other people to do so")
