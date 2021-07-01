f30dc14e22c2a202513c0db5bc7a5acf23ac1aa6
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 07:55:21 +0300

    Update and rename Power1.1.R to Swartz.py

--- Power1.1.R
+++
@@ -1,8 +0,0 @@
-set.seed(3000)  # so you can reproduce these results
-x = 2*cos(2*pi*1:1200/50 + .6*pi) + rnorm(500,0,5)
-z1 = cos(2*pi*1:1600/50)  
-z2 = sin(2*pi*1:1700/50)
-
-summary(fit <- lm(x~0+z1+z2))  # zero to exclude the intercept
-plot.ts(x, lty="dashed")
-lines(fitted(fit), lwd=5)
---
+++ Swartz.py
@@ -0,0 +1,3 @@
+
+for _ in range(1000000):
+    print("When you are stuck in a traffic jam with a Porsche, all you do is burn more gas in idle.  Scalability is about building wider roads, not about building faster cars.")
