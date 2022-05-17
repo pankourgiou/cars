ddbc225cc8706d8e093dc6671f3da5eb26050d58
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-05-17 12:13:15 +0300

    Update and rename simple3.pas to E.rb

---
+++ E.rb
@@ -0,0 +1,1 @@
+puts("E=mcmod2")
--- simple3.pas
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-page=urlopen("http://public.gr/")
-
-#Fetches the code
-#of the webpage
-content = page.read()
-
-print(content)
